df85b19d112d11437ce4913018a52cb32ca1db39
Cyber7711 <sarvareliboyev8@gmail.com>
2026-02-09 23:12:37 +0500

    please die

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,9 +1,8 @@
 import axiosClient from "./axiosClient";
 
 export const authApi = {
-  csrf: () => axiosClient.get("/auth/csrf"),
-  me: () => axiosClient.get("/auth/me"),
-  login: (body) => axiosClient.post("/auth/login", body),
-  logout: () => axiosClient.post("/auth/logout"),
+  login: (payload) => axiosClient.post("/auth/login", payload),
   refresh: () => axiosClient.post("/auth/refresh-token"),
+  logout: () => axiosClient.post("/auth/logout"),
+  me: () => axiosClient.get("/auth/me"),
 };
